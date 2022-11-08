d4c4e5819abe446db031276c0748039b340abe41
dts1234 <adas.kazmierczak@gmail.com>
2022-11-08 20:10:03 +0100

    lab materials

---
+++ Lab3-Materials/exercise/step_1/backend/hello.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+
+server = Flask(__name__)
+
+@server.route('/')
+def listBlog():
+    return '<div>Hello Trento</div>'
+if __name__ == '__main__':
+    server.run()
